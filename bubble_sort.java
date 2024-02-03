430b33b04c041c183f84c41b440439d55a4b8c4a
Anil Jangir <aniljangir51@gmail.com>
2024-02-03 16:35:53 +0530

    fibonacci series added

--- bubble_sort.java
+++ bubble_sort.java
@@ -1,9 +1,9 @@
 class BubbleSort {
     public static void main(String[] args) {
         int[] arr = { 2, 3, 4, 1, 9, 8 };
-        bubbleSort(arr);
+        bubbleSort(arr); // Time Complexity O(n^2)
         for (int i : arr) {
-            System.out.print(i+" ,");
+            System.out.print(i + " ,");
         }
     }
 
@@ -12,12 +12,12 @@ class BubbleSort {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    // Awaping
+                    // Swaping
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
             }
-         }
+        }
     }
 }
